c9f73229d215d0cea4bcb4635d4ef87ae7b3b0c5
abibernardo <153117023+abibernardo@users.noreply.github.com>
2024-09-23 21:01:28 -0300

    Add files via upload

---
+++ beteve.py
@@ -0,0 +1,213 @@
+import streamlit as st
+
+st.markdown("""
+    <h1 style='text-align: center; font-size: 60px; font-family: Palatino;'>
+        BETEVÊ
+    </h1>
+    """, unsafe_allow_html=True)
+
+# Exibindo a imagem logo abaixo do título
+st.markdown("""
+    <div style='text-align: center;'>
+        <img src="https://a.ltrbxd.com/resized/avatar/upload/5/5/2/6/7/6/8/shard/avtr-0-1000-0-1000-crop.jpg?v=a29facdc3c" 
+        style="width: 150px;">
+    </div>
+    """, unsafe_allow_html=True)
+st.write("   ")
+st.write("   ")
+st.markdown("""
+    <h1 style='text-align: center; font-size: 20px; font-family: Arial;'>
+        Opiniões, críticas, elogios e palpites sobre o melhor e o pior da televisão
+    </h1>
+    """, unsafe_allow_html=True)
+st.divider()
+st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 0;">
+        <div>
+            <h3><b>Sobre Twin Peaks:</b> De fora pra dentro.</h3>
+        </div>
+        <div>
+            <img src="https://s2.glbimg.com/aVCeHUpHz-sfAESOQfK_uNwjDE0=/smart/e.glbimg.com/og/ed/f/original/2014/10/07/meanwhile.jpg" 
+            style="width: 250px; height: auto; margin-left: 20px;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write("")
+if st.button("Ler mais", key='Laura'):
+    st.markdown("""
+        Um pescador junta seu equipamento, despede-se da esposa, e vai em direção a um rio próximo. 
+        Antes de começar a pescar, avista um objeto estranho às margens. Corre de volta para sua casa, 
+        liga para a polícia e anuncia: **"She is dead... Wrapped in plastic"**.
+
+        É assim que Twin Peaks apresenta sua principal personagem: como um cadáver.
+
+        O agente Dale Cooper é o encarregado de investigar o crime, e são por seus olhos que conhecemos o mundo de Twin Peaks. 
+        Assistimos como a tragédia abala as estruturas da então (aparentemente) pacata cidade. 
+        Por meio da investigação, conhecemos camadas e mais camadas de cada personagem e de suas relações. 
+        Entendemos os dramas, e vemos múltiplos enredos serem movidos pela eterna pergunta gritando segundo plano: 
+        **Quem matou Laura Palmer?**
+
+        **SUBVERSÕES**  
+        Twin Peaks se inicia com a aparência de um mistério policial clássico:
+        uma investigação de assassinato conduzida por um detetive carismático e personagens misteriosos potencialmente suspeitos. 
+        Aos poucos, porém, vamos percebendo uma subversão gigantesca desse gênero: trata-se de uma narrativa muito mais subjetiva e abstrata do que essa - é uma série Lynchiana, afinal.
+
+        No terceiro episódio da série, temos uma das cenas mais marcantes da história da televisão: 
+        em um sonho, agente Cooper está idoso, sentado numa poltrona numa sala toda vermelha. 
+        Na sua frente, estão sentadas duas pessoas: um homem muito pequeno, com um terno vermelho e olhos pretos. 
+        Ao seu lado, a sorridente Laura Palmer. Nesse quarto vermelho, o áudio e as imagens são gravadas ao contrário, 
+        e há um clima etéreo indescritível no ar. O homem pequeno, com as palavras soando ao contrário, diz: 
+        **"De onde nós somos, os pássaros cantam uma canção bonita, e há sempre música no ar".** 
+        E então, Laura Palmer diz: **"Sinto que conheço ela. Mas às vezes, meus braços dobram pra trás".** 
+        Uma música começa, o homem pequeno se levanta e começa a dançar. Laura Palmer, sorrindo, se levanta e caminha até o agente Cooper. 
+        Se abaixa, e cochicha algo em seu ouvido.
+    """)
+
+    # Inserir a imagem associada ao texto
+    st.image("https://static.wikia.nocookie.net/twinpeaks/images/0/0f/MFAPsitting.jpg",
+             caption="'O Homem de Outro Lugar'")
+
+    # Continuação do texto
+    st.markdown("""
+        **AS CAMADAS**
+
+        Sonhos e intuições espirituais, então, passam a fazer parte da investigação. 
+        Um astral esotérico permeia a história, permitindo que David Lynch mergulhe em representações cada vez mais abstratas de traumas, 
+        arrependimentos e autodestruição. Por meio dessas fortes abstrações, da investigação do assassinato e das relações que os personagens 
+        tinham com Laura, vai se construindo uma personagem densa, complexa, subjetiva e real, 
+        **sem que ela apareça na tela** - de fora pra dentro.
+
+        Isso é um dos fatores que torna Twin Peaks e Laura Palmer tão especiais: a fortíssima sensação que a série passa, devido a tudo que citei, 
+        de que a narrativa é **assombrada pela sua protagonista**. 
+        Em muitos momentos, é um sentimento aterrorizante - diga-se de passagem.
+    """)
+st.divider()
+st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 0;">
+        <div>
+            <h3><b>Sobre Hacks:</b> O que é se reinventar? </h3>
+        </div>
+        <div>
+            <img src="https://www.rollingstone.com/wp-content/uploads/2024/09/hacks-comedy-win.jpg?w=1581&h=1054&crop=1" 
+            style="width: 250px; height: auto; margin-left: 20px;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write("")
+if st.button("Ler mais", key='hacks'):
+    st.markdown("""
+        Nos Emmys de 2024, houve uma surpresa agradabilíssima na categoria de comédia: o prêmio de _Melhor Série_ foi para **Hacks**! 
+        Surpresa grande para as casas de aposta - mas não para mim.
+
+        Após dois Emmys de Melhor Roteiro em suas duas primeiras temporadas, os criadores Lucia Aniello, Paul W. Downs (Jimmy!) 
+        e Jen Statsky finalmente venceram o prêmio principal com uma espetacular terceira temporada, 
+        juntamente com seu terceiro Emmy de Melhor Roteiro com o episódio **mais bem avaliado da história da série** _Bulletproof_.
+
+        Isso é espantoso. Como uma série que teve que se reinventar em TODAS as transições de temporada, não só conseguiu 
+        manter seu prestígio, como elevou o nível? E o quão difícil isso é?
+
+        **GOLAÇO ATRÁS DE GOLAÇO**  
+        Rememorando, Hacks iniciou com a proposta de uma parceria aparentemente incompatível: uma jovem roteirista trabalhando para uma estrela 
+        consagrada da comédia na terceira idade. A proposta funcionou. Jean Smart e Hannah Einbinder - Deborah e Ava - mostraram-se impecáveis.
+
+        Numa relação de mutualismo igualmente fofo e hilário, Deborah e Ava, aos poucos, aceitam aprender uma com a outra, 
+        e trabalham juntas para reerguer a carreira de Deborah que, ao longo do seu convívio com a colega, se desprende do seu estilo antigo 
+        e aceita ser uma comediante mais flexível.
+
+        Na segunda temporada, Deborah decide fazer um tour de shows, e leva Ava como roteirista. 
+        O convívio torna-se cada vez mais complexo, e os efeitos que uma tem na outra - tanto os positivos, quanto os tóxicos - 
+        ficam gradualmente mais evidentes, simultaneamente dando profundidade à relação das duas e proporcionando trocas absolutamente hilárias.
+
+        Na terceira temporada, Hacks passou pela sua segunda transição notável de formato: os shows de comédia acabaram, 
+        e o coração da temporada foi a disputa de Deborah pelo cargo de apresentadora do Saturday Night Live - trabalho de seus sonhos. 
+        Ava aceita ajudar Deborah, e o resultado foi mais uma temporada absolutamente espetacular - inclusive, talvez, a melhor.
+    """)
+
+    # Inserir a imagem associada ao texto
+    st.image("https://rollingstone.com.br/media/_versions/2024/03/hacks-imagem-max-divulgacao_widelg.jpg",
+             caption="Deborah e Ava (esse ep é sensacional)")
+
+    # Continuação do texto
+    st.markdown("""
+        É raríssimo ver uma série (especialmente de comédia) passar por mudanças significativas em seu formato e ainda assim manter a excelência. 
+        Nos tempos recentes, o único comparativo que consigo pensar é em _White Lotus_ - duas temporadas de altíssimo nível, 
+        com propostas completamente diferentes uma da outra.
+
+        Ainda assim, acho o caso de Hacks ainda mais impressionante. Não houve uma mudança total na série, ou uma adição gigantesca de elenco, 
+        ou uma perda de linearidade. A série passou da forma mais orgânica possível pelas mudanças que faziam sentido, 
+        se adaptou e - assim como nossa querida Deborah fez - se reinventou, de verdade, no mais alto nível. 
+
+        Aguardo ansioso pela quarta temporada!
+    """)
+st.divider()
+st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 0;">
+        <div>
+            <h3><b>Sobre Bojack Horseman: Um personagem da metade pra baixo</b> </h3>
+        </div>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/en/8/8c/BoJack_Horseman_S06_E15_-_The_View_from_Halfway_Down.png" 
+            style="width: 450px; height: auto; margin-left: 20px;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write("")
+if st.button("Ler mais", key='Bojack'):
+    st.markdown("""
+        ## Matéria Completa     
+    """)
+st.divider()
+st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 0;">
+        <div>
+            <h3><b>Sobre Xógum:</b> Sutilezas na brutalidade</h3>
+        </div>
+        <div>
+            <img src="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/09/Xogum_-A-Gloriosa-Saga-do-Japao.png?w=1024" 
+            style="width: 300px; height: auto; margin-left: 20px;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write("")
+if st.button("Ler mais", key='xogum'):
+    st.markdown("""
+        ## Matéria Completa     
+    """)
+st.divider()
+st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 0;">
+        <div>
+            <h3><b>Sobre Baby Reindeer: O espectro da Sinceridade na arte</b> </h3>
+        </div>
+        <div>
+            <img src="https://media.glamour.com/photos/661d39dd2bf771e1378d3e83/master/w_2560%2Cc_limit/BABY%2520REINDEER%2520280324%2520Baby_Reindeer_n_S1_E2_00_20_42_07.png%2CBaby_Reindeer_n_S1_E2_00_20_42_07.jpg" 
+            style="width: 400px; height: auto; margin-left: 20px;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.write("")
+if st.button("Ler mais", key='Beberena'):
+    st.markdown("""
+        ## Matéria Completa     
+    """)
+st.divider()
+
+
+#ticker = st.sidebar.selectbox("Seções", tickers)
+
+
+#st.title("Olá!!!")
+
+
+
+#st.header("Orientações:")
+#st.markdown("""Quero colocar o máximo de análises possível, e de alguma forma colocar a opção de recebeer novos dados dos preditores para fazer predições da variavel resposta. Se vira, amigão! Algo importante é: seria legal adicionar um recurso de salvar novos objetos durante a análise. EX: salvar os ACP numa matriz para depois fazer a MANOVA.
+     #   """)
+
+
+
