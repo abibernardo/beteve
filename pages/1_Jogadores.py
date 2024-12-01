b2bc7b49e01cbf4967a50dba66e69680c4d22eed
abibernardo <153117023+abibernardo@users.noreply.github.com>
2024-12-01 14:45:50 -0300

    Update 1_Jogadores.py
    - Acréscimo: visualização do total das estatísticas na carreira
    - Estrutura: organização do código (criação de funções)
    - No futuro: colocar gráfico em função

--- pages/1_Jogadores.py
+++ pages/1_Jogadores.py
@@ -34,110 +34,210 @@ jogador_input = st.text_input("De que jogador você quer ver as estatísticas?")
 
 ###################
 # Obtendo id do jogador
-try:
-    jogador_dict = players.find_players_by_full_name(jogador_input)
-except Exception as e:
-    st.write("## Confira o nome do jogador.")
-try:
-    jogador_id = jogador_dict[0]["id"]
-except Exception as e:
-    st.write("## Confira o nome do jogador.")
 
-if jogador_dict:
+def limpar_colunas(df):
+    df = df.select(
+        pl.col("temporada"),
+        pl.col("time"),
+        pl.col("Jogos"),
+        pl.col("PTS"),
+        pl.col("REB"),
+        pl.col("AST"),
+        pl.col("STL"),
+        pl.col("BLK"),
+        pl.col("TOV"),
+        pl.col("FG%"),
+        pl.col("3 FG%"),
+        pl.col("FT%"),
+        pl.col("DREB"),
+        pl.col("OREB"),
+        pl.col("FGA"),
+        pl.col("FGM"),
+        pl.col("FG3A"),
+        pl.col("FG3M"),
+        pl.col("FTA"),
+        pl.col("FTM"),
+        pl.col("MIN"))
+    return df
+
+
+def achar_jogador(jogador_input):
+    if jogador_input:
+        try:
+            jogador_dict = players.find_players_by_full_name(jogador_input)
+        except Exception as e:
+            st.write("## Confira o nome do jogador.")
+        try:
+            jogador_id = jogador_dict[0]["id"]
+            return jogador_dict, jogador_id
+        except Exception as e:
+            st.write("## Confira o nome do jogador.")
+
+def media_por_temporada(df_carreira):
+    df_carreira = df_carreira.with_columns(
+        pl.col("SEASON_ID").alias("temporada"),
+        pl.col("SEASON_ID").str.extract(r"^(\d+)", 1).cast(pl.Int32).alias("ano"),
+        pl.col("TEAM_ABBREVIATION").alias("time"),
+        (pl.col("PTS") / pl.col("GP")).round(1),
+        (pl.col("REB") / pl.col("GP")).round(1),
+        (pl.col("AST") / pl.col("GP")).round(1),
+        (pl.col("STL") / pl.col("GP")).round(1),
+        (pl.col("BLK") / pl.col("GP")).round(1),
+        (pl.col("TOV") / pl.col("GP")).round(1),
+        (pl.col("FG_PCT") * 100).alias("FG%"),
+        (pl.col("FG3_PCT") * 100).alias("3 FG%"),
+        (pl.col("FT_PCT") * 100).alias("FT%"),
+        (pl.col("FGA") / pl.col("GP")).round(1),
+        (pl.col("FGM") / pl.col("GP")).round(1),
+        (pl.col("FG3A") / pl.col("GP")).round(1),
+        (pl.col("FG3M") / pl.col("GP")).round(1),
+        (pl.col("FTA") / pl.col("GP")).round(1),
+        (pl.col("FTM") / pl.col("GP")).round(1),
+        (pl.col("MIN") / pl.col("GP")).round(1),
+        pl.col("GP").alias("Jogos")
+    )
+    max_ano = df_carreira.select(pl.col("ano").max()).item()
+    df_temporada = df_carreira.filter(pl.col("ano") == max_ano)
+    df_temporada = df_temporada.select(
+        pl.col("PTS"),
+        pl.col("REB"),
+        pl.col("AST"),
+        pl.col("STL"),
+        pl.col("BLK"),
+        pl.col("TOV"),
+        pl.col("FG%"),
+        pl.col("3 FG%"),
+        pl.col("FT%"),
+        pl.col("FGA"),
+        pl.col("FGM"),
+        pl.col("FG3A"),
+        pl.col("FG3M"),
+        pl.col("FTA"),
+        pl.col("FTM"),
+        pl.col("MIN"),
+        pl.col("temporada"))
+    return df_carreira, df_temporada, max_ano
+
+def total_por_temporada(df_carreira):
+    df_total_carreira = df_carreira.with_columns(
+        pl.col("SEASON_ID").alias("temporada"),
+        pl.col("SEASON_ID").str.extract(r"^(\d+)", 1).cast(pl.Int32).alias("ano"),
+        pl.col("TEAM_ABBREVIATION").alias("time"),
+        pl.col("PTS").round(1),
+        pl.col("REB").round(1),
+        pl.col("AST").round(1),
+        pl.col("STL").round(1),
+        pl.col("BLK").round(1),
+        pl.col("TOV").round(1),
+        (pl.col("FG_PCT") * 100).alias("FG%"),
+        (pl.col("FG3_PCT") * 100).alias("3 FG%"),
+        (pl.col("FT_PCT") * 100).alias("FT%"),
+        pl.col("FGA").round(1),
+        pl.col("FGM").round(1),
+        pl.col("FG3A").round(1),
+        pl.col("FG3M").round(1),
+        pl.col("FTA").round(1),
+        pl.col("FTM").round(1),
+        pl.col("MIN").round(1),
+        pl.col("GP").alias("Jogos")
+    )
+    return df_total_carreira
+
+
+def total_carreira(df_medias):
+    df_totais_carreira = df_medias.with_columns(
+        pl.col("PTS"),
+        pl.col("REB"),
+        pl.col("AST"),
+        pl.col("STL"),
+        pl.col("BLK"),
+        pl.col("TOV"),
+        (pl.col("FG_PCT") * 100).alias("FG%"),
+        (pl.col("FG3_PCT") * 100).alias("3 FG%"),
+        (pl.col("FT_PCT") * 100).alias("FT%"),
+        pl.col("FGA"),
+        pl.col("FGM"),
+        pl.col("FG3A"),
+        pl.col("FG3M"),
+        pl.col("FTA"),
+        pl.col("FTM"),
+        pl.col("MIN")
+    )
+
+    df_totais_carreira = df_totais_carreira.select(
+        pl.col("PTS"),
+        pl.col("REB"),
+        pl.col("AST"),
+        pl.col("STL"),
+        pl.col("BLK"),
+        pl.col("TOV"),
+        pl.col("FG%"),
+        pl.col("3 FG%"),
+        pl.col("FT%"),
+        pl.col("FGA"),
+        pl.col("FGM"),
+        pl.col("FG3A"),
+        pl.col("FG3M"),
+        pl.col("FTA"),
+        pl.col("FTM"),
+        pl.col("MIN")
+    )
+    return df_totais_carreira
+
+
+def medias_carreira(df_medias):
+    df_medias = df_medias.with_columns(
+        (pl.col("PTS") / pl.col("GP")).round(1),
+        (pl.col("REB") / pl.col("GP")).round(1),
+        (pl.col("AST") / pl.col("GP")).round(1),
+        (pl.col("STL") / pl.col("GP")).round(1),
+        (pl.col("BLK") / pl.col("GP")).round(1),
+        (pl.col("TOV") / pl.col("GP")).round(1),
+        (pl.col("FG_PCT") * 100).alias("FG%"),
+        (pl.col("FG3_PCT") * 100).alias("3 FG%"),
+        (pl.col("FT_PCT") * 100).alias("FT%"),
+        (pl.col("FGA") / pl.col("GP")).round(1),
+        (pl.col("FGM") / pl.col("GP")).round(1),
+        (pl.col("FG3A") / pl.col("GP")).round(1),
+        (pl.col("FG3M") / pl.col("GP")).round(1),
+        (pl.col("FTA") / pl.col("GP")).round(1),
+        (pl.col("FTM") / pl.col("GP")).round(1),
+        (pl.col("MIN") / pl.col("GP")).round(1)
+    )
+    df_medias = df_medias.select(
+        pl.col("PTS"),
+        pl.col("REB"),
+        pl.col("AST"),
+        pl.col("STL"),
+        pl.col("BLK"),
+        pl.col("TOV"),
+        pl.col("FG%"),
+        pl.col("3 FG%"),
+        pl.col("FT%"),
+        pl.col("FGA"),
+        pl.col("FGM"),
+        pl.col("FG3A"),
+        pl.col("FG3M"),
+        pl.col("FTA"),
+        pl.col("FTM"),
+        pl.col("MIN"))
+    return df_medias
+
+
+
+if jogador_input:
     try:
-        #####
-        # puxando dados das bibliotecas
-        nome = jogador_dict[0]["full_name"]
+        ### PUXANDO DADOS
+        jogador_dict, jogador_id = achar_jogador(jogador_input)
         stats_carreira = playercareerstats.PlayerCareerStats(player_id=jogador_id)   # stats de todas as temporadas
-        stats_temporada = PlayerProfileV2(player_id=jogador_id)
-        #stats_temporada = stats_temporada.season_totals_regular_season
-        # stats dessa temporada
-        ##########
-        # construindo dataframes:
-
-        # CARREIRA
         df_carreira = stats_carreira.season_totals_regular_season.get_data_frame()
         df_carreira = pl.from_pandas(df_carreira)
         df_medias = stats_carreira.career_totals_regular_season.get_data_frame()
         df_medias = pl.from_pandas(df_medias)
-        df_carreira = df_carreira.with_columns(
-            pl.col("SEASON_ID").alias("temporada"),
-            pl.col("SEASON_ID").str.extract(r"^(\d+)", 1).cast(pl.Int32).alias("ano"),
-            pl.col("TEAM_ABBREVIATION").alias("time"),
-            (pl.col("PTS") / pl.col("GP")).round(1),
-            (pl.col("REB") / pl.col("GP")).round(1),
-            (pl.col("AST") / pl.col("GP")).round(1),
-            (pl.col("STL") / pl.col("GP")).round(1),
-            (pl.col("BLK") / pl.col("GP")).round(1),
-            (pl.col("TOV") / pl.col("GP")).round(1),
-            (pl.col("FG_PCT")*100).alias("FG%"),
-            (pl.col("FG3_PCT")*100).alias("3 FG%"),
-            (pl.col("FT_PCT")*100).alias("FT%"),
-            (pl.col("FGA") / pl.col("GP")).round(1),
-            (pl.col("FGM") / pl.col("GP")).round(1),
-            (pl.col("FG3A") / pl.col("GP")).round(1),
-            (pl.col("FG3M") / pl.col("GP")).round(1),
-            (pl.col("FTA") / pl.col("GP")).round(1),
-            (pl.col("FTM") / pl.col("GP")).round(1),
-            (pl.col("MIN") / pl.col("GP")).round(1)
-        )
 
-        df_medias = df_medias.with_columns(
-            (pl.col("PTS") / pl.col("GP")).round(1),
-            (pl.col("REB") / pl.col("GP")).round(1),
-            (pl.col("AST") / pl.col("GP")).round(1),
-            (pl.col("STL") / pl.col("GP")).round(1),
-            (pl.col("BLK") / pl.col("GP")).round(1),
-            (pl.col("TOV") / pl.col("GP")).round(1),
-            (pl.col("FG_PCT") * 100).alias("FG%"),
-            (pl.col("FG3_PCT") * 100).alias("3 FG%"),
-            (pl.col("FT_PCT") * 100).alias("FT%"),
-            (pl.col("FGA") / pl.col("GP")).round(1),
-            (pl.col("FGM") / pl.col("GP")).round(1),
-            (pl.col("FG3A") / pl.col("GP")).round(1),
-            (pl.col("FG3M") / pl.col("GP")).round(1),
-            (pl.col("FTA") / pl.col("GP")).round(1),
-            (pl.col("FTM") / pl.col("GP")).round(1),
-            (pl.col("MIN") / pl.col("GP")).round(1)
-        )
-        #### TEMPORADA
-        max_ano = df_carreira.select(pl.col("ano").max()).item()
-        df_temporada = df_carreira.filter(pl.col("ano") == max_ano)
-        df_temporada = df_temporada.select(
-            pl.col("PTS"),
-            pl.col("REB"),
-            pl.col("AST"),
-            pl.col("STL"),
-            pl.col("BLK"),
-            pl.col("TOV"),
-            pl.col("FG%"),
-            pl.col("3 FG%"),
-            pl.col("FT%"),
-            pl.col("FGA"),
-            pl.col("FGM"),
-            pl.col("FG3A"),
-            pl.col("FG3M"),
-            pl.col("FTA"),
-            pl.col("FTM"),
-            pl.col("MIN"),
-            pl.col("temporada"))
-        df_medias = df_medias.select(
-            pl.col("PTS"),
-            pl.col("REB"),
-            pl.col("AST"),
-            pl.col("STL"),
-            pl.col("BLK"),
-            pl.col("TOV"),
-            pl.col("FG%"),
-            pl.col("3 FG%"),
-            pl.col("FT%"),
-            pl.col("FGA"),
-            pl.col("FGM"),
-            pl.col("FG3A"),
-            pl.col("FG3M"),
-            pl.col("FTA"),
-            pl.col("FTM"),
-            pl.col("MIN"))
+        ### TRATANDO DADOS:  df_carreira = media por temporada
+        df_total_carreira = total_por_temporada(df_carreira)
+        df_carreira, df_temporada, max_ano = media_por_temporada(df_carreira)
         ppg = df_temporada.item(0, 0)
         rpg = df_temporada.item(0, 1)
         apg = df_temporada.item(0, 2)
@@ -155,7 +255,11 @@ if jogador_dict:
         ftm = df_temporada.item(0, 14)
         mpg = df_temporada.item(0, 15)
         temporada = df_temporada.item(0, 16)
-        ### medias carreira
+
+        ### totais carreira
+        totais_carreira = total_carreira(df_medias)
+        ### medias da carreira
+        df_medias = medias_carreira(df_medias)
         ppg_carreira = df_medias.item(0, 0)
         rpg_carreira = df_medias.item(0, 1)
         apg_carreira = df_medias.item(0, 2)
@@ -165,27 +269,8 @@ if jogador_dict:
         fg_carreira = df_medias.item(0, 6)
         fg3_carreira = df_medias.item(0, 7)
         ft_carreira = df_medias.item(0, 8)
-        dados_carreira_tela = df_carreira.select(
-            pl.col("temporada"),
-            pl.col("time"),
-            pl.col("PTS"),
-            pl.col("REB"),
-            pl.col("AST"),
-            pl.col("STL"),
-            pl.col("BLK"),
-            pl.col("TOV"),
-            pl.col("FG%"),
-            pl.col("3 FG%"),
-            pl.col("FT%"),
-            pl.col("FGA"),
-            pl.col("FG3A"),
-            pl.col("FTA"),
-            pl.col("MIN")
-        )
 
-        ##### carreer high
-        career_high = stats_temporada.career_highs.get_data_frame()
-        career_high = pl.from_pandas(career_high)
+
 
     except Exception as e:
         st.write("")
@@ -194,7 +279,8 @@ else:
 
 
 
-if jogador_dict and jogador_input:
+if jogador_input:
+    nome = jogador_dict[0]["full_name"]
     if max_ano > 2023:
         st.write(f"# {nome}: {temporada}")
         cols = st.columns(5)
@@ -224,11 +310,9 @@ if jogador_dict and jogador_input:
             </p>
         </div>
         """.format(fg=fg, fg3=fg3, ft=ft), unsafe_allow_html=True)
-        #st.write(f"# stats por temporada")
-        #st.dataframe(dados_carreira_tela)
+
     else:
         st.write(f"# {nome}")
-        st.dataframe(df_medias)
         cols = st.columns(5)
         stats_visual = [
             (f"{ppg_carreira}", "pts"),
@@ -257,11 +341,39 @@ if jogador_dict and jogador_input:
                 </div>
                 """.format(fg=fg_carreira, fg3=fg3_carreira, ft=ft_carreira), unsafe_allow_html=True)
     st.divider()
-    col1, col2 = st.columns(2)
+    col1, col15, col2 = st.columns(3)
     with col1:
         stat = st.selectbox(
-            'Qual estatística quer analisar?',
+            'Estatística de interesse',
             stats, key='stats')
-    fig = px.line(df_carreira, x="ano", y=stat)
-    st.plotly_chart(fig)
+    with col2:
+        tipo_stat = st.radio("Forma da estatística", ["Por jogo", "Total"])
+    media_carreira = df_medias.select(pl.col(stat).first()).item()
+    total_carreira_stat = totais_carreira.select(pl.col(stat).first()).item()
+
+    if tipo_stat == "Por jogo":
+        fig = px.line(df_carreira, x="ano", y=stat)
+        fig.update_layout(
+            xaxis=dict(
+                tickmode="array",  # Define os ticks manualmente
+                tickvals=df_carreira["ano"].to_list(),  # Usa os valores únicos da coluna 'ano'
+            )
+        )
+        st.plotly_chart(fig)
+        st.markdown(f"<p style='font-size:18px; font-weight:bold; text-align:center;'>{media_carreira:.1f} {stat} de média na carreira</p>", unsafe_allow_html=True)
+        st.divider()
+        dados_exibidos = limpar_colunas(df_carreira)
+    elif tipo_stat == "Total":
+        fig = px.line(df_total_carreira, x="ano", y=stat)
+        fig.update_layout(
+            xaxis=dict(
+                tickmode="array",  # Define os ticks manualmente
+                tickvals=df_total_carreira["ano"].to_list(),  # Usa os valores únicos da coluna 'ano'
+            )
+        )
+        st.plotly_chart(fig)
+        st.markdown(f"<p style='font-size:18px; font-weight:bold; text-align:center;'>{total_carreira_stat} {stat} na carreira</p>", unsafe_allow_html=True)
+        st.divider()
+        dados_exibidos = limpar_colunas(df_total_carreira)
+    st.dataframe(dados_exibidos)
 st.divider()
