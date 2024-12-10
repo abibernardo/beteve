e334cf33b85e0a2d607d2f66eef9841921547f04
abibernardo <153117023+abibernardo@users.noreply.github.com>
2024-12-09 21:12:38 -0300

    ocultando erros (try-catch)

--- pages/3_Últimos Jogos.py
+++ pages/3_Últimos Jogos.py
@@ -73,24 +73,24 @@ def achar_jogador(jogador_input):
         try:
             jogador_dict = players.find_players_by_full_name(jogador_input)
         except Exception as e:
-            st.write("## Confira o nome do jogador.")
+            st.write(" ")
         try:
             jogador_id = jogador_dict[0]["id"]
             return jogador_dict, jogador_id
         except Exception as e:
-            st.write("## Confira o nome do jogador.")
+            st.write(" ")
 
 def achar_time(time_input):
     if time_input:
         try:
             time_dict = teams.find_team_by_abbreviation(time_input)
         except Exception as e:
-            st.write("## Confira o nome do jogador.")
+            st.write(" ")
         try:
             time_id = time_dict["id"]
             return time_id
         except Exception as e:
-            st.write("## Confira o nome do jogador.")
+            st.write(" ")
 
 
 
@@ -137,134 +137,140 @@ st.title("Estatísticas dos últimos jogos")
 
 
 st.divider()
-
-col1, col2 = st.columns(2)
-with col1:
-    input = st.text_input("De que jogador você quer ver as estatísticas?")
-    if input:
-        jogador_dict, jogador_id = achar_jogador(input)
-    else:
-        jogador_dict, jogador_id = achar_jogador('harden')
-    nome = jogador_dict[0]["full_name"]
-    df_jogos = achar_jogo_jogador(jogador_id)
-
-
-with col2:
-    ultimos_jogos = st.number_input("Há quantos jogos?", min_value=1, step=1)
-
-
-df_jogos = df_jogos.head(ultimos_jogos)
-df_jogos = (
-        df_jogos
-            .with_row_count(name="jogo")
-            .with_columns((pl.lit(df_jogos.height) - pl.col("jogo")).alias("jogo"))
-    )
+try:
+    col1, col2 = st.columns(2)
+    with col1:
+        input = st.text_input("De que jogador você quer ver as estatísticas?", placeholder="James Harden")
+        if input:
+            try:
+                jogador_dict, jogador_id = achar_jogador(input)
+            except Exception as e:
+                st.write(" ")
+        else:
+            jogador_dict, jogador_id = achar_jogador('harden')
+        nome = jogador_dict[0]["full_name"]
+        df_jogos = achar_jogo_jogador(jogador_id)
 
 
-df_jogos = df_jogos.with_columns(
-        pl.col("GAME_DATE").alias("DATA"),
-        pl.col("WL").alias("W/L"),
-        pl.col("PTS"),
-        pl.col("REB"),
-        pl.col("AST"),
-        pl.col("STL"),
-        pl.col("BLK"),
-        pl.col("TOV"),
-        (pl.col("FG_PCT") * 100).alias("FG%"),
-        (pl.col("FG3_PCT") * 100).alias("3 FG%"),
-        (pl.col("FT_PCT") * 100).alias("FT%"),
-        pl.col("FGA"),
-        pl.col("FGM"),
-        pl.col("FG3A"),
-        pl.col("FG3M"),
-        pl.col("FTA"),
-        pl.col("FTM"),
-        pl.col("MIN")
-    )
+    with col2:
+        ultimos_jogos = st.number_input("Há quantos jogos?", min_value=1, step=1)
 
 
-#### Médias
-# Calculando médias com apenas uma casa decimal
-ppg = round(df_jogos["PTS"].mean(), 1)
-rpg = round(df_jogos["REB"].mean(), 1)
-apg = round(df_jogos["AST"].mean(), 1)
-spg = round(df_jogos["STL"].mean(), 1)
-bpg = round(df_jogos["BLK"].mean(), 1)
-
-# Calculando porcentagens FG, FG3, e FT com apenas uma casa decimal
-fg = round((df_jogos["FGM"].sum() / df_jogos["FGA"].sum()) * 100, 1)
-fg3 = round((df_jogos["FG3M"].sum() / df_jogos["FG3A"].sum()) * 100, 1)
-ft = round((df_jogos["FTM"].sum() / df_jogos["FTA"].sum()) * 100, 1)
-
-# Configuração dos valores para exibição
-
-stats_visual = [
-    (f"{ppg}", "pts"),
-    (f"{rpg}", "reb"),
-    (f"{apg}", "ast"),
-    (f"{spg}", "stl"),
-    (f"{bpg}", "blk")
-]
-
-# Exibindo os valores no layout do Streamlit
+    df_jogos = df_jogos.head(ultimos_jogos)
+    df_jogos = (
+            df_jogos
+                .with_row_count(name="jogo")
+                .with_columns((pl.lit(df_jogos.height) - pl.col("jogo")).alias("jogo"))
+        )
 
 
-st.write(f'## Últimos {ultimos_jogos} jogos de {nome}')
-if ultimos_jogos:
-    cols = st.columns(5)
-    for col, (value, label) in zip(cols, stats_visual):
-        with col:
-            st.markdown(f"""
-                <div style="text-align: center; background-color: #091836; padding: 10px; border-radius: 10px;">
-                    <p style="margin: 0; font-size: 24px; font-weight: bold; color: white;">{value}</p>
-                    <p style="margin: 0; font-size: 14px; color: gray;">{label}</p>
-                </div>""", unsafe_allow_html=True)
-    st.markdown(f"""
-        <div style="text-align: center; margin-top: 20px;">
-            <p style="font-size: 28px; font-weight: bold; color: #333;">
-                {fg:.1f}% FG / {fg3:.1f}% 3 PTS / {ft:.1f}% FT
-            </p>
-        </div>
-    """, unsafe_allow_html=True)
+    df_jogos = df_jogos.with_columns(
+            pl.col("GAME_DATE").alias("DATA"),
+            pl.col("WL").alias("W/L"),
+            pl.col("PTS"),
+            pl.col("REB"),
+            pl.col("AST"),
+            pl.col("STL"),
+            pl.col("BLK"),
+            pl.col("TOV"),
+            (pl.col("FG_PCT") * 100).alias("FG%"),
+            (pl.col("FG3_PCT") * 100).alias("3 FG%"),
+            (pl.col("FT_PCT") * 100).alias("FT%"),
+            pl.col("FGA"),
+            pl.col("FGM"),
+            pl.col("FG3A"),
+            pl.col("FG3M"),
+            pl.col("FTA"),
+            pl.col("FTM"),
+            pl.col("MIN")
+        )
 
 
-st.divider()
+    #### Médias
+    # Calculando médias com apenas uma casa decimal
+    ppg = round(df_jogos["PTS"].mean(), 1)
+    rpg = round(df_jogos["REB"].mean(), 1)
+    apg = round(df_jogos["AST"].mean(), 1)
+    spg = round(df_jogos["STL"].mean(), 1)
+    bpg = round(df_jogos["BLK"].mean(), 1)
 
-df_jogos_display = df_jogos.select(
-        pl.col("DATA"),
-        pl.col("MATCHUP"),
-        pl.col("W/L"),
-        pl.col("PTS"),
-        pl.col("REB"),
-        pl.col("AST"),
-        pl.col("STL"),
-        pl.col("BLK"),
-        pl.col("TOV"),
-        pl.col("FG%"),
-        pl.col("3 FG%"),
-        pl.col("FT%"),
-        pl.col("FGA"),
-        pl.col("FGM"),
-        pl.col("FG3A"),
-        pl.col("FG3M"),
-        pl.col("FTA"),
-        pl.col("FTM"),
-        pl.col("MIN"))
-st.dataframe(df_jogos_display)
-st.divider()
-col11, col12 = st.columns(2)
-if ultimos_jogos > 1:
-    with col11:
-        stat = st.selectbox(
-                    'Estatística de interesse',
-                    stats, key='stats')
-    st.write(f"## {stat} de {nome}: {ultimos_jogos} últimos jogos")
-    fig = px.line(df_jogos, x="jogo", y=stat)
-    fig.update_layout(
-            xaxis=dict(
-            tickmode="array",  # Define os ticks manualmente
-            tickvals=df_jogos["jogo"].to_list(),))  # Usa os valores únicos da coluna 'ano'
-    st.plotly_chart(fig)
-    if stat not in ["FG%", "3 FG%", "FT%"]:
-        media = round(df_jogos_display[stat].mean(), 1)
-        st.write(f"**Média de {media}**")
+    # Calculando porcentagens FG, FG3, e FT com apenas uma casa decimal
+    fg = round((df_jogos["FGM"].sum() / df_jogos["FGA"].sum()) * 100, 1)
+    fg3 = round((df_jogos["FG3M"].sum() / df_jogos["FG3A"].sum()) * 100, 1)
+    ft = round((df_jogos["FTM"].sum() / df_jogos["FTA"].sum()) * 100, 1)
+
+    # Configuração dos valores para exibição
+
+    stats_visual = [
+        (f"{ppg}", "pts"),
+        (f"{rpg}", "reb"),
+        (f"{apg}", "ast"),
+        (f"{spg}", "stl"),
+        (f"{bpg}", "blk")
+    ]
+
+    # Exibindo os valores no layout do Streamlit
+
+
+    st.write(f'## Últimos {ultimos_jogos} jogos de {nome}')
+    if ultimos_jogos:
+        cols = st.columns(5)
+        for col, (value, label) in zip(cols, stats_visual):
+            with col:
+                st.markdown(f"""
+                    <div style="text-align: center; background-color: #091836; padding: 10px; border-radius: 10px;">
+                        <p style="margin: 0; font-size: 24px; font-weight: bold; color: white;">{value}</p>
+                        <p style="margin: 0; font-size: 14px; color: gray;">{label}</p>
+                    </div>""", unsafe_allow_html=True)
+        st.markdown(f"""
+            <div style="text-align: center; margin-top: 20px;">
+                <p style="font-size: 28px; font-weight: bold; color: #333;">
+                    {fg:.1f}% FG / {fg3:.1f}% 3 PTS / {ft:.1f}% FT
+                </p>
+            </div>
+        """, unsafe_allow_html=True)
+
+
+    st.divider()
+
+    df_jogos_display = df_jogos.select(
+            pl.col("DATA"),
+            pl.col("MATCHUP"),
+            pl.col("W/L"),
+            pl.col("PTS"),
+            pl.col("REB"),
+            pl.col("AST"),
+            pl.col("STL"),
+            pl.col("BLK"),
+            pl.col("TOV"),
+            pl.col("FG%"),
+            pl.col("3 FG%"),
+            pl.col("FT%"),
+            pl.col("FGA"),
+            pl.col("FGM"),
+            pl.col("FG3A"),
+            pl.col("FG3M"),
+            pl.col("FTA"),
+            pl.col("FTM"),
+            pl.col("MIN"))
+    st.dataframe(df_jogos_display)
+    st.divider()
+    col11, col12 = st.columns(2)
+
+    if ultimos_jogos > 1:
+        with col11:
+            stat = st.selectbox(
+                        'Estatística de interesse',
+                        stats, key='stats')
+        st.write(f"## {stat} de {nome}: {ultimos_jogos} últimos jogos")
+        fig = px.line(df_jogos, x="jogo", y=stat)
+        fig.update_layout(
+                xaxis=dict(
+                tickmode="array",  # Define os ticks manualmente
+                tickvals=df_jogos["jogo"].to_list(),))  # Usa os valores únicos da coluna 'ano'
+        st.plotly_chart(fig)
+        if stat not in ["FG%", "3 FG%", "FT%"]:
+            media = round(df_jogos_display[stat].mean(), 1)
+            st.write(f"**Média de {media}**")
+except Exception as e:
+    st.write("### Confira o nome do jogador.")
