ead46e676304d8326055452a3381e14acf988f6d
abibernardo <153117023+abibernardo@users.noreply.github.com>
2026-05-10 02:15:36 -0300

    Update beteve.py

--- beteve.py
+++ beteve.py
@@ -3,7 +3,6 @@ from nba_api.stats.endpoints import PlayerProfileV2
 import streamlit as st
 from nba_api.stats.static import players
 import pandas as pd
-import polars as pl
 import plotly.express as px
 import plotly.graph_objects as go
 
