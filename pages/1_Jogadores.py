8fef9144b4aec38d79755603e695635b716dc013
abibernardo <153117023+abibernardo@users.noreply.github.com>
2026-05-10 01:57:08 -0300

    Update 1_Jogadores.py

--- pages/1_Jogadores.py
+++ pages/1_Jogadores.py
@@ -7,6 +7,30 @@ import pandas as pd
 import polars as pl
 import plotly.express as px
 
+import requests
+
+from nba_api.stats.library.http import NBAStatsHTTP
+
+session = requests.Session()
+
+session.headers.update({
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'x-nba-stats-origin': 'stats',
+    'x-nba-stats-token': 'true',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.nba.com/',
+    'Origin': 'https://www.nba.com',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+})
+
+NBAStatsHTTP._session = session
+NBAStatsHTTP.TIMEOUT = 15
+
 st.set_page_config(layout="wide")
 
 # =========================================================
